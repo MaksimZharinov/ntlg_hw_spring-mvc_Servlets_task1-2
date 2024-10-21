1a49cefd457d16c078d63b1b1d02c07a95d62276
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-10-22 02:54:41 +0700

    change PostRepository to create a REPO

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,25 +1,50 @@
 package ru.netology.repository;
 
+import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
-// Stub
 public class PostRepository {
-  public List<Post> all() {
-    return Collections.emptyList();
-  }
 
-  public Optional<Post> getById(long id) {
-    return Optional.empty();
-  }
+    private final ConcurrentHashMap<Long, Post> REPO = new ConcurrentHashMap<>();
+    private long countPosts = 0L;
 
-  public Post save(Post post) {
-    return post;
-  }
+    public List<Post> all() {
+        if (REPO.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return (List<Post>) REPO.values();
+    }
 
-  public void removeById(long id) {
-  }
+    public Optional<Post> getById(long id) {
+        if (REPO.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.ofNullable(REPO.get(id));
+    }
+
+    public Post save(Post post) {
+        if (post.getId() != 0) {
+            if (!REPO.containsKey(post.getId())) {
+                throw new NotFoundException("Post with such ID does not exist");
+            }
+            return REPO.replace(post.getId(), post);
+        }
+        post.setId(countPosts + 1);
+        REPO.put(post.getId(), post);
+        countPosts++;
+        return post;
+    }
+
+    public void removeById(long id) {
+        if (!REPO.containsKey(id)) {
+            throw new NotFoundException("Post with such ID does not exist");
+        }
+        REPO.remove(id);
+        countPosts--;
+    }
 }
